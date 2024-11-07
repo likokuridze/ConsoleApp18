2927b4fce6d4b9699e0df813ea1e52aaca92a52c
LikoKuridze <lkuridze@credo.ge>
2024-11-08 01:59:38 +0400

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "kira";
+        int age = 25;
+
+        Person person = new Person();
+        person.Name = name;
+        person.Age = age;
+
+        Console.WriteLine($"Name: {person.Name}");
+        Console.WriteLine($"Age: {person.Age}");
+
+        person.Name = "lui";
+        person.Age = 30;
+
+        Console.WriteLine($"Updated Name: {person.Name}");
+        Console.WriteLine($"Updated Age: {person.Age}");
+    }
+}
